085045db8663768c73c1df9a5c810e770cc53c32
Aldric Ducreux <neogeekmo@live.fr>
2026-09-15 15:14:31 +0200

    Update data files for latest builds and sources

--- Data/classic/ModifierTrees.lua
+++ Data/classic/ModifierTrees.lua
@@ -1,5 +1,5 @@
 -- This file is generated. Do not edit it by hand.
--- Source: https://wago.tools/db2/ModifierTree (build 1.15.9.69547)
+-- Source: https://wago.tools/db2/ModifierTree (build 1.15.9.69722)
 local lib = _G.LibTaxiData_Internal
 if not lib then return end
 if not lib.Client or lib.Client.dataSet ~= "classic" then return end
--- Data/classic/TaxiNodes.lua
+++ Data/classic/TaxiNodes.lua
@@ -1,11 +1,11 @@
 -- This file is generated. Do not edit it by hand.
--- Source: https://wago.tools/db2/TaxiNodes (build 1.15.9.69547)
+-- Source: https://wago.tools/db2/TaxiNodes (build 1.15.9.69722)
 local lib = _G.LibTaxiData_Internal
 if not lib then return end
 if not lib.Client or lib.Client.dataSet ~= "classic" then return end
 
 lib.Source = {
-    build = "1.15.9.69547",
+    build = "1.15.9.69722",
     profile = "classic",
     dataSet = "classic",
     gameType = "classic",
