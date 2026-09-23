55f46a53f1cc8523bfca15e34dd1ef3ac973a824
Neogeekmo <28711160+Neogeekmo@users.noreply.github.com>
2026-09-23 04:52:16 +0000

    Update taxi data for all

--- Data/forever/SupportingData.lua
+++ Data/forever/SupportingData.lua
@@ -1,5 +1,5 @@
 -- This file is generated. Do not edit it by hand.
--- Source: https://wago.tools/db2/ChrRaces + ContentTuning (build 1.60.1.69913)
+-- Source: https://wago.tools/db2/ChrRaces + ContentTuning (build 1.60.1.69977)
 local lib = _G.LibTaxiData_Internal
 if not lib then return end
 if not lib.Client or lib.Client.dataSet ~= "forever" then return end
@@ -40,5 +40,4 @@ lib.RaceBits = {
 }
 
 lib.ContentTuning = {
-    [6102] = { minLevel = 50, maxLevel = 60, minLevelOffset = 0, maxLevelOffset = 0 },
 }
--- Data/forever/TaxiNodes.lua
+++ Data/forever/TaxiNodes.lua
@@ -1,11 +1,11 @@
 -- This file is generated. Do not edit it by hand.
--- Source: https://wago.tools/db2/TaxiNodes (build 1.60.1.69913)
+-- Source: https://wago.tools/db2/TaxiNodes (build 1.60.1.69977)
 local lib = _G.LibTaxiData_Internal
 if not lib then return end
 if not lib.Client or lib.Client.dataSet ~= "forever" then return end
 
 lib.Source = {
-    build = "1.60.1.69913",
+    build = "1.60.1.69977",
     profile = "forever",
     dataSet = "forever",
     gameType = "camelot",
